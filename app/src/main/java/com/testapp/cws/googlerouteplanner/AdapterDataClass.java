b4c1e4d215b3d220011fdc2e895834cf48ad1c82
cwstech_developers <cwstech.developers@gmail.com>
2018-01-30 18:36:44 +0530

    ajaya commited

--- app/src/main/java/com/testapp/cws/googlerouteplanner/AdapterDataClass.java
+++ app/src/main/java/com/testapp/cws/googlerouteplanner/AdapterDataClass.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  */
 
 public class AdapterDataClass {
-    /////ajaya commited
+    /////ajaya commited nbhghghghhgghgh
     public static ArrayList<LatLng> arrayList;
     public static ArrayList<String> step_array;
     public static ArrayList<String> html_array;
